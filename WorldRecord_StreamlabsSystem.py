d5aed35681a308ac7e6e07962be826c87b01838f
IracanePaul <58866076+IracanePaul@users.noreply.github.com>
2019-12-13 17:49:48 -0500

    Rewrite World_Record
    Change the code to utilize more of the Twitch API, and better sections of the Speedrun.com API.
    
    Unfortunately, this no longer allows you to search for games that aren't being played.
    
    Pulls information from the Twitch API to get the name of the game being played, as well as the title in order to search the speedrun API for a list of that games categories.  From there it checks the existing categories against the game name to look for a match in the title, before grabbing the first place run in that game + category combination, and outputting the game, category, time and runner name from the world record run.
    
    Furthermore made it easier to add games to the list of accepted games (SpeedrunGame / Category Extension functions need to be updated with an if statement), and added the ability to search for category extension games.

--- WorldRecord_StreamlabsSystem.py
+++ WorldRecord_StreamlabsSystem.py
@@ -1,189 +1,164 @@
-ScriptName = "ddWorldRecord"
+ScriptName = "WR"
 Website = "https://speedrun.com/"
-Description = "Pulls World Record for Category when called with 'commandName Category'" #See Line 7
+Description = "Pulls the World Record for a Category when called with 'commandName Category'" #See Line 15
 Creator = "AuroTheAzure"
-Version = "1.0.0"
+Version = "1.1.0"
 
-commandName = "!wr"
-cooldown = 1
-global game
-game = ""
-# Script will pull your current game (On Twitch, Sorry Mixer) when the command is run.
-# Currently has help for SMO, Celeste, and SM64 :")
+''' 
+Notes:
+    Currently only works with top 50 runners.  For some weird reason code can't pull back past runner...58 or so, so 50 to be safe.
+    Please make sure the name on like 18 matches your speedrun.com username
+    Cooldown on line 16 is in seconds.
+    Match Category in Twitch Title to the Tab in hte main board. :) (Any% | World Peace | All Red Berries | 70 Star etc.
+'''
 
-import json             # For quickly / easily parsing data from speedrun.com/
-from re import split    # For splitting times for formatting from 1H1M1S to 1:01:01
+commandName = "!wr"     #String to start the command
+cooldown = 1            #Cooldown of the command in seconds
 
-global Live
-headers = { 'Client-ID': 'umyyc7qkoxedtjir8wo50iinz6qzs8'}
+import datetime         #For converting time from a number of seconds to a human readable format (1:23:45.67)
+import json             #For quickly / easily parsing data from speedrun.com/
+
+
+headers = {'Client-ID': 'umyyc7qkoxedtjir8wo50iinz6qzs8'}  #No touchey
 
 def Init():
-    global Live
-    Live = 0
     return
 
 
 def Execute(data):
-    global Live
-    global game
-    if Parent.IsLive() and Live == 0:
-        game = getGame()
-        if game != "":  #If we have a game, we don't come back in here
-            Live = 1
-    if data.GetParam(0) != commandName or Parent.IsOnCooldown(ScriptName, commandName):
+    if data.GetParam(0) != commandName or Parent.IsOnCooldown(ScriptName, commandName) or not Parent.IsLive():
         #If we're not called with our command name, or we're on cooldown, quit.
         #Parent.Log("!wr", "The game is {}".format(game))
         return
-    elif data.GetParamCount() < 2:
-        game = getGame()
-        #If we're called without a category, Inform the user that we need a category, then quit.
-        weError(game)
-        return
     else:
-        #Parent.Log("!wr", "Called with {} arguments.".format(data.GetParamCount()))
-        if data.GetParamCount() == 3:
-            game = data.GetParam(1)
-            #Parent.Log("!wr", "Game is {} Should be {}".format(game,data.GetParam(1)))
-            category = data.GetParam(2)
-        else:
-            game = getGame()
-            category = data.GetParam(1)
-        url, category = getUrl(category.upper(), game)
-        response = Parent.GetRequest(url, {})                   #Send a get request
-        page = json.loads(response)                             #Parse the get request to a json
-        if page['status'] != 200:
-            send_message("There was a problem grabbing the {} leaderboard for {} ,"
-                         " are you sure you got the format the same way as speedrun? (_'s instead of spaces?)".format(category, game))
+        game, title = getGame()
+        if game == -1 and title == -1:
+            send_message("There was an issue grabbing the game / title from Twitch.")
             return
-        page = json.loads(page['response'])                     #Grab the data portion
-        WorldRecordRun = page["data"]["runs"][0]                #Grab the specific run
-        #Parent.Log("!wr", str(WorldRecordRun))                 #Log the json of the run
-        Time = WorldRecordRun['run']['times']['primary'][2::]   #Grab the time (1H12M38.423S format)
-        TimeParsed = "" 
-            
-        TimeParsed = split('[A-Za-z]', Time)                    #Split time into digits
-        TimeParsed = TimeParsed[0:-1]                           #Remove the trailing section due to split
-        for i, each in enumerate(TimeParsed):                   #This loop sucks.
-            if i == 0:                                          #Ignore the first place (Most Significant time unit)
-                pass                                            #Then pad the rest to 2 characters
-            else:
-                TimeParsed[i] = each.rjust(2,'0')
-                
-        TimeParsed = ":".join(TimeParsed)                       #Join the times to get 1:42:13
-        User = WorldRecordRun['run']['players'][0]['id']        #Grab User ID
-        UserResponse = Parent.GetRequest("https://speedrun.com/api/v1/users/{}".format(User), {})
-        UserPage = json.loads(UserResponse)                     #Get speedrun.com data for user code
-        UserPage = json.loads(UserPage['response'])             #Make speedrun.com data parsable
-        username = UserPage['data']['names']['international']   #Grab international username for current WR holder
-        
-        send_message("The current World Record for {} {} is {} held by {}".format(game, category, TimeParsed, username))
-	
-        Parent.AddCooldown(ScriptName, commandName, cooldown)
-    return
+        gameURL = SpeedrunGame(game)
+        url, category = getCategories(gameURL, title)
+        if url == -5 and category == -5:            # This means the category wasn't found in the twitch title
+            gameURL = CategoryExtensions(game)
+            url, category = getCategories(gameURL, title)
+            if url == -5 and category == -5:
+                send_message("I couldn't find any categories in the stream title.")
+                return
+        elif url == -2 and category == -2:          # This means the category page failed to load.
+            send_message("Contact the creator, because your game is not currently supported.") #Gotta get more work somehow :)
+            return
+        runs = getRuns(url)
+    
+        WRrun = runs[0]['run'] #WR run
+        Parent.Log("!wr", str(WRrun))
+        Parent.Log("!wr", str(type(WRrun)))
+        runner_name = getRunnerName(WRrun['players'][0]['id'])
+        Time = WRrun['times']['primary_t']
+        TimeParsed = datetime.timedelta(seconds=Time)
+        TimeString = str(TimeParsed)
+        while TimeString[0] == '0' or TimeString[0] == ':':
+            TimeString = TimeString[1::]
+        send_message("The WR in {} {} is {} held by {}.".format(game, category, TimeString, runner_name))
+	return
 
 
 def Tick():
     return
 
 
-def getUrl(category, title):
-    #Title is Game, Category is type of run, in all caps
-    #Parent.Log("!wr", "We're in the GetURL section.")
-    #Parent.Log("!wr", "The game is {} Category is {}".format(title, category))
-    Display = category
+def SpeedrunGame(TwitchGameName):
+    #TwitchGameName is Game according to Twitch
+    if TwitchGameName == "Super Mario Odyssey":     #SMO Main board
+        return "smo"
+    elif TwitchGameName == "Super Mario 64":        #SM64 Main Board
+        return "sm64"
+    elif TwitchGameName == "Celeste":               #Celeste Main Board
+        return "celeste"
+    elif TwitchGameName == "Super Mario Sunshine":
+        return "sms"
+    elif TwitchGameName == "Yu-Gi-Oh! Forbidden Memories":
+        return "yugiohfm"
+
+def CategoryExtensions(TwitchGameName):
+    #TwitchGameName is the Game according to Twitch
+    if TwitchGameName == "Super Mario Odyssey":     #SMO Category Extensions
+        return "smoce"
+    elif TwitchGameName == "Super Mario 64":        #SM64 Category Extensions
+        return "sm64memes"
+    elif TwitchGameName == "Celeste":               #Celeste Category Extensions
+        return "celeste_category_extensions"
+    elif TwitchGameName == "Super Mario Sunshine":
+        return "smsce"
+    elif TwitchGameName == "Yu-Gi-Oh! Forbidden Memories":
+        return "yugiohfmextensions"
+
+def getRunnerName(speedrunner_id):
+    #Get the runners ID from speedrun.com to query the speedrun API less.
+    Parent.Log("!wr", "Pulling from speedrun.com/api/v1/users/{}".format(speedrunner_id))
+    runnerPage = Parent.GetRequest("https://speedrun.com/api/v1/users/{}".format(speedrunner_id), {})
+    runnerPage = json.loads(runnerPage)                     #Get speedrun.com data for user code
+    Parent.Log("!wr", "Runner Page: {}".format(runnerPage))
+    runnerPage = json.loads(runnerPage['response'])
+    speedrunner_name = runnerPage['data']['names']['international']   #Grab international username for current WR holder
+    Parent.Log("!wr", "Runner Name is {}".format(speedrunner_name))
+    return speedrunner_name
+	
     
-    if title == "Super Mario Odyssey":      #SMO Main board
-        title = "smo"
-        #Parent.Log("!wr", "Game is {}".format(title))
-        if category == "ANY" or category == "ANY%":
-            category = "Any"
-            Display = "Any%"
-        elif category == "WP":
-            category = "World_Peace"
-            Display = "World Peace"
-        elif category == "DARK":
-            category = "Dark_Side"
-            Display = "Dark Side"
-        elif category == "DARKER":
-            category = "Darker_Side"
-            Display = "Darker Side"
-        elif category == "100" or category == "100%":
-            category = "100"
-            Display = "100%"
-        elif category == "AM":
-            category = "All_Moons"
-            Display = "All Moons"
-    elif title == "Super Mario 64":     #SM64 Main Board
-        title = "sm64"
-        #Parent.Log("!wr", "Game is {} Part Duex".format(title))
-        if category == "120":
-            category = "120_Star"
-            Display = "120 Stars"
-        elif category == "70":
-            category = "70_Star"
-            Display = "70 Star"
-        elif category == "16":
-            category = "16_Star"
-            Display = "16 Star"
-        elif category == "1":
-            category = "1_Star"
-            Display = "1 Star"
-        elif category == "0":
-            category = "0_Star"
-            Display = "0 Star"
-    elif title == "Celeste":           #Celeste Main Board
-        title = "celeste"
-        #Parent.Log("!wr", "Game is {}".format(title))
-        if category == "ANY" or category == "ANY%":
-           category = "Any"
-           Display = "Any%"
-        elif category == "TE" or category == "TRUE":
-            category = "True_Ending"
-            Display = "True Ending"
-        elif category == "ARB":
-            category = "All_Red_Berries"
-            Display = "All Red Berries"
-        elif category == "AH":
-            category = "All_Hearts"
-            Display = "All Hearts"
-        elif category == "AC":
-            category = "All_Chapters"
-            Display = "All Chapters"
-        elif category == "100" or category == "100%":
-            category = "100"
-            Display = "100%"
-    url = 'https://speedrun.com/api/v1/leaderboards/{}/category/{}'.format(title, category)
-    Parent.Log("!wr", "Attemping to grab {}'s {} leaderboard".format(title, category))
-    return url, Display
-
-
 def getGame():
+    #Pulls the json blob from twitch, and returns the Game (As a string), and the Title of the stream.
     Parent.Log("!wr", "Connecting to Twitch API to pull the game you're playing.")
     r = Parent.GetRequest("https://api.twitch.tv/helix/streams?user_login={}".format(Parent.GetChannelName()), headers)
-    rJson = json.loads(r)
-    if rJson['status'] == 200:
+    rJson = json.loads(r)       #Parent.GetRequest pulls information back as a string, we're converting it to a json object
+    if rJson['status'] == 200:  # If successful response
         Live = 1
         GameJson = json.loads(rJson['response'])
+        #Parent.Log("!wr", "GameJson: {}".format(GameJson))
         Game_Code = GameJson['data'][0]['game_id']
         #Parent.Log("!wr", str(Game_Code))
+        #Twitch api stores games as an ID (######) we need a human readable game name
         r = Parent.GetRequest("https://api.twitch.tv/helix/games?id={}".format(Game_Code), headers)
         r = json.loads(json.loads(r)['response'])
-        global game 
         game = r['data'][0]['name']
+        title = GameJson['data'][0]['title']
+        Parent.Log("!wr", "Title is : {}".format(title))
         Parent.Log("!wr", "Game is {}".format(game))
-    return game
+        return game, title
+    return -1, -1
 
 
-def weError(gamename):
-    if gamename == "Super Mario Odyssey":
-        send_message("Please include the category name. (Any, WP, Dark, Darker, AM, 100%)")
-        return
-    elif gamename == "Celeste":
-        send_message("Please include the category name. (Any, True, ARB, AH, AC, 100)")
-    elif gamename == "Super Mario 64":
-        send_message("Please include the Star count. (120, 70, 16, 1, 0)")
-        
-        
+def getCategories(game, TwitchTitle):
+    #Uses the speedrun API to get a list of category names for the game being run
+    #This functions returns a link to the leaderboard page, and the name of the category to print later...
+    #Debating returning the blob
+    Parent.Log("!wr", "Getting list of category names from speedrun.com.")
+    CategoryPage = Parent.GetRequest("https://speedrun.com/api/v1/games/{}/categories".format(game), {})
+    CategoryPage = json.loads(CategoryPage)
+    if CategoryPage['status'] == 200:
+        CategoryPage = json.loads(CategoryPage['response'])
+        TwitchTitleUpper = TwitchTitle.upper()
+        for each in CategoryPage['data']:
+            Parent.Log("!wr", "checking for {} in Title.".format(each['name']))
+            if each['name'].upper() in TwitchTitleUpper:
+                for link in each['links']:
+                    if link['rel'] == "records":
+                        return link['uri'], each['name']
+    else:   #Failed to load the categories page for the game
+        Parent.Log("!wr", "{} has no categories page.".format(game))
+        return -2, -2
+    #If we found no matching category in the title of the stream
+    Parent.Log("!wr", "Error pulling the categories for {}.".format(game))
+    return -5, -5
+    
+def getRuns(LeaderboardURL):
+    #Pull the list of runs from the speedrun api
+    Leaderboard = Parent.GetRequest(LeaderboardURL, {})
+    Leaderboard = json.loads(Leaderboard)
+    Parent.Log("!wr", str(Leaderboard))
+    if Leaderboard['status'] != 200:
+        Parent.Log("There was an issue getting the leaderboard {}.".format(LeaderboardURL))
+        return "Blame speedrun.com for being broken. :)"
+    Leaderboard = json.loads(Leaderboard['response'])
+    return Leaderboard['data'][0]['runs']
+
 def send_message(message):
     Parent.SendStreamMessage(message)
     return
